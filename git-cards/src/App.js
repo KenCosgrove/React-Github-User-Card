780365b3f883560380114e7faec2508dbce8aac3
AJ Gambino <aj.gambino7@gmail.com>
2020-09-02 14:07:04 -0400

    card component

--- git-cards/src/App.js
+++ git-cards/src/App.js
@@ -2,13 +2,31 @@ import React from 'react';
 import './App.css';
 import GitCard from './GitCard'
 
-function App() {
-  return (
-    <div className="App">
-      <h2>hii</h2>
-      <GitCard />
-    </div>
-  );
+
+class App extends React.Component {
+ 
+  render(){
+    return(
+            
+                <div>
+                    <GitCard />
+                </div>
+            
+        
+    )
 }
+
+
+
+
+}
+
+
+
+
+
+
+
+
 
 export default App;
--- git-cards/src/GitCard.js
+++ git-cards/src/GitCard.js
@@ -1,44 +1,81 @@
 import React from 'react'
 import axios from 'axios'
 
-const profiles = []
 
 class GitCard extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            profiles
-        }
-        console.log("constructor")
-    }
-
-    componentDidMount() {
-        console.log("cdm")
-        axios.get("").then(res=> {
+   constructor(){
+    super()
+    this.state = {
+        users: [],
+        search: '',
+        error: ''
+      };
+   }
+      componentDidMount() {
+          console.log("cdm")
+        axios
+          .get('https://api.github.com/users/KenCosgrove')
+          .then(res => {
             this.setState({
-                profiles: res.data
-            })
-        }).catch(err => {
-            console.log("error")
-        })
-    }
-    render(){
-        return(
-                this.state.profiles.map(item =>(
-                    <div>
-                        <img src={item.avatar_url} alt=""></img>
-                        <h2>{item.name}</h2>
-                        <h3>{item.login}</h3>
-                        <h3>Followers: {item.followers}</h3>
-                        <h3>Following: {item.following}</h3>
-                        <h3>Location: {item.location}</h3>
-                        <h3>Bio: {item.bio}</h3>
-                    </div>
-                ))
-            
-        )
+              users: res.data
+            });
+          })
+          .catch(err => console.log("error"));
+      }
+    
+      handleChanges = e => {
+        this.setState({
+          search: e.target.value
+        });
+      };
+    
+      findUsers = e => {
+        e.preventDefault();
+        axios
+          .get(`https://api.github.com/users/${this.state.search}`)
+          .then(res => {
+            this.setState({
+              users: res.data,
+              error: ''
+            });
+            console.log(this.state.users)
+          })
+          .catch(err => {
+            this.setState({
+              error: 'Could not find that user. Please try again'
+            });
+          });
+      };
+    
+      render() {
+        return (
+        <div className="App">
+            <h1>Github User Cards</h1>
+            <input
+              type="text"
+              value={this.state.search}
+              onChange={this.handleChanges}
+            />
+            <button onClick={this.findUsers}>Find User</button>
+            <div>
+                {/*  {this.state.users.map(user => (
+                <div>
+                  <img src={user.avatar_url} alt="" />
+                  <h2>{user.name}</h2>
+                  <h3>{user.login}</h3>
+                  <p>Followers: {user.followers}</p>
+                  </br>
+                  <p>Following: {user.following}</p>
+                </div>
+                ))}  */}
+            </div>    
+        </div>
+          
+        );
+      }
     }
     
-}
+  
+  
 
 export default GitCard
